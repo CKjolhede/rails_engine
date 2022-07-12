21ccd4c9a4261ab37176016ba3960aa59405db26
Chris Kjolhede <ckjolhede@gmail.com>
2022-07-12 13:41:03 -0600

    create ItemsController with #index and call to render json from serializer

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::ItemsController < ApplicationController
+
+  def index
+    @merchant = Merchant.find(params[:merchant_id])
+    render json: ItemSerializer.new(@merchant.items.all)
+  end
+end
