1610dd3a97015b5817786248fac7e07b3d14f305
Chris Kjolhede <ckjolhede@gmail.com>
2022-07-12 08:27:51 -0600

    test for getting response for just one merchant

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -7,8 +7,16 @@ RSpec.describe "Merchants API" do
     expect(response).to be_successful
     
     merchants = JSON.parse(response.body, symbolize_names: true)  
-  
-
-
+  # binding.pry
   end
+
+  it 'can get one merchant' do
+    get "/api/v1/merchants/42"
+
+    expect(response).to have_https_status(200)
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchant[:data][:attributes]).to include(:name)
+  end
+
 end
