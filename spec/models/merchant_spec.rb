6ce20ca4a80ad7330d0cd7cf6a66408202c1434f
Chris Kjolhede <ckjolhede@gmail.com>
2022-07-12 07:53:02 -0600

    test relationships and validations of Merchant and Item models

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
   describe "relationships and validations" do
-    it (should have_many :items)
+    it { should have_many :items}
 
-    it (should validate_presence_of :name)
+    it {should validate_presence_of :name}
   end
 end
