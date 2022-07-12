2a4a08969599d879b625ea12f5b6633d519cd7fa
Chris Kjolhede <ckjolhede@gmail.com>
2022-07-11 21:34:32 -0600

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+
+# This code is how to utilize factory_bot to create seed data (not part of this project)
+# require 'factory_bot_rails'
+# 10.times do
+#   FactoryBot.create :book
+# end
+
+
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+cmd = "pg_restore --verbose --clean --no-acl --no-owner -h localhost -U $(whoami) -d rails-engine_development db/data/rails-engine-development.pgdump"
+puts "Loading PostgreSQL Data dump into local database with command:"
+puts cmd
+system(cmd)
