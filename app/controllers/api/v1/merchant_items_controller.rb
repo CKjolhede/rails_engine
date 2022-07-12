7c6379ee761a70ad22a669af851a6f3c4b5a71ac
Chris Kjolhede <ckjolhede@gmail.com>
2022-07-12 14:11:09 -0600

    create merchant item controller to handle namespaced item calls

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -1,6 +1,7 @@
-class Api::V1::ItemsController < ApplicationController
+class Api::V1::MerchantItemsController < ApplicationController
 
   def index
+    # binding.pry
     @merchant = Merchant.find(params[:merchant_id])
     render json: ItemSerializer.new(@merchant.items.all)
   end
