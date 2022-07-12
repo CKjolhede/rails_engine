4eb7eae256da955f85265a3c349bb5559d24fa91
Chris Kjolhede <ckjolhede@gmail.com>
2022-07-12 07:55:15 -0600

    get all merchant passing Postman test 1a get all merchants

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -3,11 +3,12 @@ require 'rails_helper'
 RSpec.describe "Merchants API" do
   it "sends a list of all merchants" do
 
-    get '/api/v1/merchants'
+    get "/api/v1/merchants"
+    expect(response).to be_successful
+    
+    merchants = JSON.parse(response.body, symbolize_names: true)  
+  
 
-expect(response).to be_successful
-response_body = JSON.parse(response.body, {symbolize_names: true})  
-merchants = response_body[:data]
 
   end
 end
