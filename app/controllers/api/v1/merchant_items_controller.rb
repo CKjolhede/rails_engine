0150d68e8229a77d54f85da34d010707c27b9ddf
Chris Kjolhede <ckjolhede@gmail.com>
2022-07-12 14:44:11 -0600

    add sad path tests to get all merchants

---
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::MerchantItemsController < ApplicationController
+
+  def index
+    # binding.pry
+    @merchant = Merchant.find(params[:merchant_id])
+
+    render json: ItemSerializer.new(@merchant.items.all)
+  end
+end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -2,18 +2,33 @@ require 'rails_helper'
 
 RSpec.describe "Merchants API" do
   
-  it "sends a list of all merchants" do
-    create_list(:merchant, 5)
-    
-    get "/api/v1/merchants"
-    expect(response).to be_successful
-    
-    response_body = JSON.parse(response.body, symbolize_names: true)  
-    merchants = response_body[:data]
-    expect(merchants).to be_a Array
-    expect(merchants.count).to eq(5)
-    expect(merchants[0][:attributes]).to include(:name)
-   
+  context 'sends a list of all merchants' do
+    it "on the happy path" do
+      create_list(:merchant, 5)
+      
+      get "/api/v1/merchants"
+      expect(response).to be_successful
+      
+      response_body = JSON.parse(response.body, symbolize_names: true)  
+      # expect(response_body).to be_a Hash
+      merchants = response_body[:data]
+      expect(merchants).to be_a Array
+      expect(merchants.count).to eq(5)
+        merchants.each do |merchant|
+          expect(merchant[:attributes]).to include(:name)
+        end
+    end
+    it "on the sad path" do
+      create_list(:merchant, 5)
+      
+      get "/api/v1/merchants"
+      
+      response_body = JSON.parse(response.body, symbolize_names: true)  
+      expect(response_body).to include(:data) #sad path because without serializer :data absent
+      merchants = response_body[:data]
+      expect(merchants).to_not include(:created_at)
+      expect(merchants).to_not include(:updated_at)
+    end
   end
   
   xit 'can get one merchant' do
@@ -23,6 +38,7 @@ RSpec.describe "Merchants API" do
     expect(response).to have_https_status(200)
    response_body = JSON.parse(response.body, symbolize_names: true)
     merchant = response_body[:data]
+    expect(merchant.count).to eq(3)
     expect(merchant[:attributes]).to include(:name)
   end
 end
