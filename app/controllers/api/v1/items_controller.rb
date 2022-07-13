86ead6695fbadeb74efbdc660b030d77206c0196
Chris Kjolhede <ckjolhede@gmail.com>
2022-07-13 02:15:51 -0600

    create #create and #destroy within Items controller; include strong params

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -8,4 +8,18 @@ class Api::V1::ItemsController < ApplicationController
     item = Item.find(params[:id])
     render json: ItemSerializer.new(item), status: :ok
   end
+
+  def create
+    render json: ItemSerializer.new(Item.create(item_params)), status: :created
+  end
+
+  def destroy
+    render json: Item.destroy(params[:id]), status: :no_content
+  end
+
+private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+  end
 end
