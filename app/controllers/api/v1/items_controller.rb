e1b30cc3b713929c23e37693ee9f4000110a8d51
Chris Kjolhede <ckjolhede@gmail.com>
2022-07-13 00:18:38 -0600

    create #show for items controller; include json rendering fxn using serializer

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::ItemsController < ApplicationController
   def index
     render json: ItemSerializer.new(Item.all), status: :ok
   end
+
+  def show
+    item = Item.find(params[:id])
+    render json: ItemSerializer.new(item), status: :ok
+  end
 end
