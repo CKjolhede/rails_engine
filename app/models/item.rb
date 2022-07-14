1ae6bae1706e6fe2de61c825af7c21eb20a4ce1e
Chris Kjolhede <ckjolhede@gmail.com>
2022-07-14 17:15:18 -0600

    create #search in merchant model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :merchant
   validates_presence_of :name, :description, :unit_price, :merchant_id
+
+  def self.search(name)
+    where('name ILIKE ?',"%#{name}%").order(:name)
+  end
 end
