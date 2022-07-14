590c592234553742257e45e21eb764366b393bb3
Chris Kjolhede <ckjolhede@gmail.com>
2022-07-14 09:26:47 -0600

    add routed to merchant show

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index, :show] do
-        resources :items, only: [:index], controller: "merchant_items" #, action: :index
+        resources :items, only: [:index], controller: "merchant_items" 
       end
-      resources :items
+      resources :items do
+        resources :merchants, only: [:show], controller: "merchant_items", action: :show
+      end
     end
   end
 end
