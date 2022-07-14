e9543ad589dc9170149673848e38215f2f62b8f9
Chris Kjolhede <ckjolhede@gmail.com>
2022-07-14 17:13:29 -0600

    add #find method to merchant controller

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -8,4 +8,14 @@ class Api::V1::MerchantsController < ApplicationController
   def show
     render json: MerchantSerializer.new(Merchant.find(params[:id])), status: :ok
   end
+
+  def find
+    search_word = Merchant.search(params[:name])
+    if search_word.nil?
+      render json: {data: {error: "No merchant found with #{params[:name]} in the name"}}, status: 200
+    else
+      render json: MerchantSerializer.new(search_word), status: 200
+      # binding.pry
+    end
+  end
 end
