d0a27914c082d00c4c4c6bd29d69411965a1f910
Chris Kjolhede <ckjolhede@gmail.com>
2022-07-14 09:30:55 -0600

    add merch item show

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -2,7 +2,12 @@ class Api::V1::MerchantItemsController < ApplicationController
 
   def index
     # binding.pry
-    @merchant = Merchant.find(params[:merchant_id])
-    render json: ItemSerializer.new(@merchant.items.all)
+    merchant = Merchant.find(params[:merchant_id])
+    render json: ItemSerializer.new(merchant.items.all)
+  end
+
+  def show
+    merchant = Merchant.find(params[:id])
+    render json: MerchantSerializer.new(merchant), status: :ok
   end
 end
