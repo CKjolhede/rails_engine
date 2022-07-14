340d0343af60c3717d96dd95a051442c31e8fe8c
Chris Kjolhede <ckjolhede@gmail.com>
2022-07-14 17:16:57 -0600

    handroll routes to merchant/find and item/find_all

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      get '/merchants/find', to: 'merchants#find'
+      get '/items/find_all', to: 'items#find_all'
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index], controller: "merchant_items" 
       end
 
       get '/items/:id/merchant', to: 'merchant_items#show'
-
       resources :items do
         # resources :merchants, only: [:show], controller: "merchant_items", action: :show
       end
